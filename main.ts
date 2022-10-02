634cbc8f2d06cdf8a3f472b0cb8bcd6d4748f57f
Waleedh Nihal Ahmed <waleedhnihal@outlook.com>
2022-10-02 19:21:16 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let sonar = 0
+basic.showIcon(IconNames.Asleep)
+cuteBot.forward()
 basic.forever(function () {
-	
+    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (sonar > 2 && sonar < 15) {
+        cuteBot.motors(0, 0)
+        for (let index = 0; index < 4; index++) {
+            music.playTone(880, music.beat(BeatFraction.Quarter))
+        }
+        basic.pause(2000)
+        cuteBot.motors(randint(-50, -100), 0)
+        basic.pause(500)
+    } else if (false) {
+    	
+    } else {
+        cuteBot.forward()
+    }
 })
